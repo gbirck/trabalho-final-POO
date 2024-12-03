7121d3427aecc3cd677493f6e7ed3419e634d342
Benrardo Radin <bernardoradin2004@gmail.com>
2024-12-02 23:27:03 -0300

    Telas e cadastros Interface

--- src/trabalho/TrabalhoFinal.java
+++ src/trabalho/TrabalhoFinal.java
@@ -1,9 +1,12 @@
 package trabalho;
 
+import view.home;
+
 public class TrabalhoFinal {
 
     public static void main(String[] args) {
-        
+        home Home = new home();
+        Home.setVisible(true);
     }
     
 }
